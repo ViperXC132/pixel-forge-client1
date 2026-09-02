e99c4d6afa341b64860042b8f6e82d5081ffc07e
Viper Davis <viperxc2204@gmail.com>
2026-09-02 20:08:51 +0100

    Make particle limiter apply immediately

--- src/main/java/com/pixelforge/module/modules/performance/ParticleLimiterModule.java
+++ src/main/java/com/pixelforge/module/modules/performance/ParticleLimiterModule.java
@@ -2,10 +2,18 @@ package com.pixelforge.module.modules.performance;
 
 import com.pixelforge.module.Category;
 import com.pixelforge.module.Module;
+import net.minecraft.particle.ParticlesMode;
 
+/** Lightweight particle limiter using Minecraft's supported particle quality setting. */
 public class ParticleLimiterModule extends Module {
-
-    public ParticleLimiterModule() {
-        super("Particle Limiter", "Limits the number of particles spawned", Category.PERFORMANCE);
+    private ParticlesMode previous;
+    public ParticleLimiterModule(){super("Particle Limiter","Limits particle density to reduce render load",Category.PERFORMANCE);}
+    @Override public void onEnable(){
+        super.onEnable();
+        try{previous=mc.options.getParticles().getValue();mc.options.getParticles().setValue(ParticlesMode.MINIMAL);}catch(Throwable ignored){}
+    }
+    @Override public void onDisable(){
+        try{if(previous!=null)mc.options.getParticles().setValue(previous);previous=null;}catch(Throwable ignored){}
+        super.onDisable();
     }
 }
